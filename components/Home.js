9ea537b98dbfe66ab13c78bdd65a63f24d35d250
lionel <argv0002@gmail.com>
2024-11-22 15:05:51 +0100

    travail sur le home

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,31 @@
 import styles from '../styles/Home.module.css';
-
+import LastTweets from './LastTweets';
+import { useState,useEffect } from 'react';
 function Home() {
+
+      const [valueTweets,setValueTweets] = useState('');
+     
+
+
+    const handleTweets = ()=>{
+     fetch('http://localhost:3000/tweets/add',{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ tweet: valueTweets})
+     }).then(response => response.json())
+     .then(data=>{
+        setValueTweets('')
+      console.log(data)
+     })
+    }
+
+
   return (
-    <div>
-      <main className={styles.main}>
-      </main>
+    <div className={styles.container}>
+           <h1 className={styles.title}>Home</h1>
+          <input type='text' onChange={(e)=> setValueTweets(e.target.value)}  value={valueTweets} placeholder="what's up?" />
+          <button onClick={()=>handleTweets()} className={styles.btn_tweets}>Tweet</button>
+          <LastTweets />
     </div>
   );
 }
---
+++ pages/home.js
@@ -0,0 +1,8 @@
+import LastTweets from '../components/LastTweets'
+import Home from '../components/Home';
+
+function HomePage() {
+  return <Home />
+}
+
+export default HomePage;
